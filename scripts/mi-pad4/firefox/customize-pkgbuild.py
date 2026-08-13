58562b4c7a65475b3d41585878bc8f4b1c55de61
E7G <55611737+E7G@users.noreply.github.com>
2026-08-13 20:07:05 +0800

    fix: keep Firefox V4L2 GPU presentation stable

--- scripts/mi-pad4/firefox/customize-pkgbuild.py
+++ scripts/mi-pad4/firefox/customize-pkgbuild.py
@@ -52,7 +52,8 @@ replace_once(
 )
 replace_once(
     '  echo -n "$_google_api_key" >google-api-key\n',
-    '  patch -Np1 <"$srcdir/../0002-mi-pad4-clover-direct-dmabuf.patch"\n\n'
+    '  patch -Np1 <"$srcdir/../0002-mi-pad4-clover-direct-dmabuf.patch"\n'
+    '  patch -Np1 <"$srcdir/../0003-mi-pad4-clover-full-egl-damage.patch"\n\n'
     '  echo -n "$_google_api_key" >google-api-key\n',
 )
 replace_once(
