43acd2910075e41a2088a9a64dda525cab35f067
E7G <55611737+E7G@users.noreply.github.com>
2026-08-17 00:59:34 +0800

    fix: Firefox patches EOF and Hangover build simplify
    - Fix 0002 EOF newline
    - Rewrite 0003 as minimal patch
    - Simplify Hangover build: remove llvm-mingw download, use system mingw

--- scripts/mi-pad4/windows/build-hangover.sh
+++ scripts/mi-pad4/windows/build-hangover.sh
@@ -11,16 +11,6 @@ STAGE="$PWD/$OUTPUT_DIR"
 
 rm -rf "$BUILD_DIR" "$OUTPUT_DIR"
 
-# Download llvm-mingw prebuilt toolchain
-LLVM_MINGW_VER="20260616"
-curl -L \
-  "https://github.com/mstorsjo/llvm-mingw/releases/download/${LLVM_MINGW_VER}/llvm-mingw-${LLVM_MINGW_VER}-ucrt-ubuntu-24.04-aarch64.tar.xz" \
-  -o /tmp/llvm-mingw.tar.xz
-sudo mkdir -p /opt/llvm-mingw
-sudo tar -xJf /tmp/llvm-mingw.tar.xz \
-  --strip-components=1 \
-  -C /opt/llvm-mingw
-
 # Clone Hangover with submodules
 git clone \
   --recurse-submodules \
@@ -34,7 +24,6 @@ git submodule update --init --recursive
 # Build Wine with Hangover patches
 mkdir -p wine/build
 cd wine/build
-export PATH="/opt/llvm-mingw/bin:$PATH"
 ../configure \
   --disable-tests \
   --with-mingw=clang \
@@ -42,22 +31,6 @@ export PATH="/opt/llvm-mingw/bin:$PATH"
   --prefix=/usr/local
 make -j"$(nproc)"
 make DESTDIR="$STAGE" install
-
-# Build FEX ARM64EC DLL for x86_64 Windows apps
-cd "$PWD/../../fex"
-mkdir -p build_ec
-cd build_ec
-cmake \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_TOOLCHAIN_FILE=../Data/CMake/toolchain_mingw.cmake \
-  -DENABLE_LTO=False \
-  -DMINGW_TRIPLE=arm64ec-w64-mingw32 \
-  -DBUILD_TESTS=False \
-  ..
-cmake --build . -j"$(nproc)" --target arm64ecfex
-install -Dm755 \
-  Bin/libarm64ecfex.dll \
-  "$STAGE/usr/local/lib/wine/aarch64-windows/libarm64ecfex.dll"
 
 cd ../..
 
